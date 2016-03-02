c59416253447117d8f8a7e92f85986d77bbd5557
Sasha Mile Rudan <mprinc@gmail.com>
2016-03-03 00:53:22 +0100

    fix(fixed name):

--- mappp.js
+++ mappp.js
@@ -25,8 +25,8 @@
 */
 
 var BaseMap, isArray, isFunction, isObjectLit, mappp, toArrayPairs,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
   __slice = [].slice;
 
 mappp = {};
